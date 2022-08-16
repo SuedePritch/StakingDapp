a1c005ee800c7c4c1fea598b5459198e41a73701
James Pritchard <james.edward.pritchard@gmail.com>
2022-08-16 13:46:54 -0400

    wrote contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {version:"0.8.0"},
+  paths:{
+    artifacts: './client/src/artifacts'
+  }
 };
